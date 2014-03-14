98dbdf415da4cbb05b0672117898af3832b576c8
Hiroyuki Kurosawa <kh99mail@gmail.com>
2014-03-14 23:28:06 +0900

    Initial Commit

---
+++ HiddenFiles/HiddenFiles.h
@@ -0,0 +1,15 @@
+//
+//  HiddenFiles.h
+//  HiddenFiles
+//
+//  Created by hiroyukik on 2014/03/14.
+//  Copyright (c) 2014年 Hiroyuki Kurosawa. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface HiddenFiles : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ HiddenFiles/HiddenFiles.m
@@ -0,0 +1,17 @@
+//
+//  HiddenFiles.m
+//  HiddenFiles
+//
+//  Created by hiroyukik on 2014/03/14.
+//  Copyright (c) 2014年 Hiroyuki Kurosawa. All rights reserved.
+//
+
+#import "HiddenFiles.h"
+
+@implementation HiddenFiles
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
